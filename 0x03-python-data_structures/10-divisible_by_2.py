0b30531f310eebd1d0e50d3685b658e14c3903e4
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-12 16:14:11 -0700

    10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div = []
+    for n in my_list:
+        if my_list[n] % 2 == 0:
+            div.append(True)
+        else:
+            div.append(False)
+    return div
