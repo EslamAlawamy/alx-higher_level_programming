8882c6dae374510f7125c39e8860790fb294c924
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-06-12 22:58:43 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for f in range(len(matrix)):
             for s in range(len(matrix[f])):
+                if s != 0:
                 print("{:d}".format(matrix[f][s]), end=" ")
             print()
