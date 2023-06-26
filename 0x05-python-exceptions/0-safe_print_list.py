ca2eed79100837a65ded4e8c46cdc10959468fc4
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-06-26 15:26:20 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if copy < x:
-                print(f"{copy}", end="")
+                print(f"{my_list[copy]}", end="")
                 copy += 1
         print()
     except (TypeError):
