0cd36e85c57e76d3da7b1f3feb8c553deb8d38e2
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-26 05:18:43 -0700

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    copy = 0
+    try:
+        for i in my_list:
+            if copy < x:
+                print(f"{x}", end="")
+                copy += 1
+        print()
+    except:
+        pass
+    return copy
