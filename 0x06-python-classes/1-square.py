1aaec751a14e31bb18b5f73029e6138ba7761575
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-27 11:27:31 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" the main class module """
+
+
+class Square:
+    """ the class square """
+    def __init__(self, size):
+        """
+        init function to initalizes the square
+
+        Args: the size square
+        """
+        self.__size = size
