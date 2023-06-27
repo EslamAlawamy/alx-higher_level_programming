d29e2d83a3cacc8d466bc20ce3ce1335a045ab6a
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-27 11:43:07 -0700

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" the main class module """
+
+
+class Square:
+    """ the class of square """
+    def __init__(self, size=0):
+        """
+        init function to initialize the square
+        Args: size(int): the size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size == size
