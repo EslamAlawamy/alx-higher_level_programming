8eadedf030ac302f75e1e97cd01ead4e3ec2efd8
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-06-27 17:33:15 -0700

    103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" the module """
+class MagicClass:
+    """ the class """
+    def __init__(self, radius):
+        if type(radius) is not (int, float):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+    def area(self):
+        return self.__radius ** 2 * math.pi
+    def circumference(self):
+        return 2 * math.pi * __radius
