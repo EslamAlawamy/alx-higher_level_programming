433d664e07718451262d1d50b77937cfc185e8ae
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-07-03 16:17:28 -0700

    1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Real definition of a rectangle """
+
+
+class Rectangle:
+    """ Rectangle class """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def width(self):
+        """ width getter of rectangle"""
+        return self.__width
+
+
+    def width(self, value):
+        """ width setter pf rectangle """
+        if type(value) is not (int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter the height of rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter of the height of rectangle """
+        if type(value) is not (int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
