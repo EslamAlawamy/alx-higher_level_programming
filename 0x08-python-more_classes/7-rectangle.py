74f2fbb91399a30d084cbb1d6c06e4e18df4b7cc
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-07-03 18:59:26 -0700

    7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """ rectangle class """
     number_of_instances = 0
+    print_symbol = "#"
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -51,8 +52,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return ((self.__width * "#" + "\n") * (self.__height - 1)
-                + (self.__width * "#"))
+        return ((self.__width * f"{self.print_symbol}" + "\n") * (self.__height)).rstrip()
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
