a8d23dda2bb9d8e594f750f073a2d2aa91b46520
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-07-05 01:27:07 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,7 +12,9 @@ class TestMaxInteger(unittest.TestCase):
         """ check if it's none """
         self.assertEqual(max_integer([]), None)
 
-    
+    def test_repeted_number(self):
+        """ repeated number """
+        self.assertEqual(max_integer([369, 369, 369, 369]), 369)
 
     def test_one_element(self):
         """ one element """
