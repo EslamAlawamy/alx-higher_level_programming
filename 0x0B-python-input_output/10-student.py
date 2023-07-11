77d3abebc6669a0aebafaad64e693d725e268dfa
EslamAlawamy <eslamalawamy.com@gmail.com>
2023-07-11 10:24:41 -0700

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Student to JSON with filter """
+
+
+class Student:
+    """ student class """
+    def __init__(self, first_name, last_name, age):
+        """ initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ that retrieves a dictionary representation of a Student """
+        if attrs is None or type(attrs) is not list:
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
