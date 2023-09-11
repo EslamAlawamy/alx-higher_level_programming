230823801e88c78fffb70698f151748cbf08a025
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-12 02:25:46 +0300

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,8 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0, s; i < size; i++) {
+  let s;
+  for (let i = 0; i < size; i++) {
     s = '';
     for (let j = 0; j < size; j++) {
       s += 'X';
