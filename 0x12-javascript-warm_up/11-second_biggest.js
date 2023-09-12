6f053ef8cedaee528fcf17ccbd4b809eaa076113
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-12 12:14:51 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-if (args.length <= 1) {
-  console.log(0);
-} else {
-  args.sort((a, b) => b - a);
-  console.log(args[1]);
-}
-
+const secondBiggest = process.argv
+  .slice(2)
+  .map(Number)
+  .sort((a, b) => b - a)[1];
+console.log(secondBiggest === undefined ? 0 : secondBiggest);
