a6b0e786ac72c251d509b197b90a0299758a6626
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-14 16:58:38 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ import MySQLdb
 
 if __name__ == '__main__':
     """ lists all states with a name starting with N """
-  db = MySQLdb.connect(host="localhost",
+
+    db = MySQLdb.connect(host="localhost",
                          user=argv[1], passwd=argv[2], db=argv[3], port=3306)
   cur = db.cursor()
   cur.execute("SELECT * FROM states \
