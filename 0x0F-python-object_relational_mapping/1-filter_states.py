208e2674b10db43ecab19022752ba634071a870f
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-14 16:58:03 +0300

    Update 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 1. Filter states """
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    """ lists all states with a name starting with N """
+  db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+  cur = db.cursor()
+  cur.execute("SELECT * FROM states \
+              WHERE name LIKE 'N%' \
+              ORDER BY states.id ASC")
+  rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
