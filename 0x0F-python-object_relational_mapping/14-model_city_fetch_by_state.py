108203d7b5ed6adfd612ea51ff961ceabfb8d705
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-09-15 05:33:13 -0700

    14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 14. Cities in state """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ = "__main__":
+    """ prints all City objects from the database hbtn_0e_14_usa """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    for state, city in st_cty:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" base city """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ city class """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = column(Integer, nullable=False, ForeignKey("states.id"))
