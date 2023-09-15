77f5e1c0121705b026bcb4005d9ed036586aa31d
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-15 14:47:46 +0300

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 8. First state """
+""" 10. Get a state """
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    """ prints the first State object from the database hbtn_0e_6_usa """
+    """ State object with name passed as argument from hbtn_0e_6_usa """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
