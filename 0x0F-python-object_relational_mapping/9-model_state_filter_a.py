1f37c5c671895f98ee4dbf00bd1ac81e8f66727d
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-15 14:35:54 +0300

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,16 +7,12 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    """
-    lists all State objects that contain the letter
-    a from the database hbtn_0e_6_usa
-    """
+    """ State objects that contain the letter a from database hbtn_0e_6_usa """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name.like('%a%'))
-                          .order_by(State.id).first()
+    state = session.query(State).filter(State.name.like('%a%')).order_by(State.id).first()
     if state is None:
         print("Nothing")
     else:
