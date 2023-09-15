fc2e44b0335f64859d52463839b33fa72dc4b76b
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-15 15:39:10 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ base city """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
