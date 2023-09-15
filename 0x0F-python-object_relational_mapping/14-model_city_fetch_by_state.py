9dba75c6d2ed3d31e9bd9afeabe1fafbb8ae079c
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-09-15 15:36:59 +0300

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-""" 14. Cities in state """
+"""
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
+"""
+
 from sys import argv
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-if __name__ = "__main__":
-    """ prints all City objects from the database hbtn_0e_14_usa """
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
