27325f42e2bd9d13189b236cf1cf0852d76903df
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-09-15 05:05:47 -0700

    11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" 11. Add a new state """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """ adds the State object “Louisiana” to the database hbtn_0e_6_usa """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    newState = State(name="Louisiana")
+    session.add(newState)
+    session.commit()
+    print(states.id)
