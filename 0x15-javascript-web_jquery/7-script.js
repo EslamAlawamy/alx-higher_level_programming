4254cd7e7bc6db15d02f71f29321f22f42b8ef79
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2023-11-05 14:05:57 +0200

    Update 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,5 @@
-const $ = window.$;
-$.get('https://swapi.co/api/people/5/?format=json', function (data, textStatus) {
-  $('DIV#character').text(data.name);
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', (data) => {
+    $('DIV#character').text(data.name);
+  });
 });
